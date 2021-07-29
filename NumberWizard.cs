a5bf05944756604164eff106a0606166b9ad6ab5
Kevin Bernard <kevos.directos@gmail.com>
2021-07-29 17:24:56 +0200

    Addition of the rules of the game

--- NumberWizard.cs
+++ NumberWizard.cs
@@ -7,7 +7,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        Debug.Log("Hello Player");
+        Debug.Log("We will play a game, you will think of a number, and I have to guess it.");
+        Debug.Log("The highest number you can pick is 1000.");
+        Debug.Log("The lowest number you can pick is 1.");
+        Debug.Log("Is your number higher or lower than 500 ?");
+        Debug.Log("Arrow Up = Higher - Arrow Down = Lower - Enter = Correct");
+
     }
 
     // Update is called once per frame
