4957fe424098ff17e6b2ea3f90d4fdf8b3d46207
Kevin Bernard <kevos.directos@gmail.com>
2021-07-29 17:30:04 +0200

    Rescoping

--- NumberWizard.cs
+++ NumberWizard.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
     // Start is called before the first frame update
     void Start()
     {
-        int max = 1000;
-        int min = 1;
-        int guess = 500;
-
         Debug.Log("Hello Player");
         Debug.Log("We will play a game, you will think of a number, and I have to guess it.");
         Debug.Log("The highest number you can pick is " + max + ".");
